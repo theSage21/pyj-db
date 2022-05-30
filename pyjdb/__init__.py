d62ebd086ec7504d86b94495154487e68c82d92a
Shivam Purohit <37547983+shvamabps@users.noreply.github.com>
2022-05-30 09:48:31 +0530

    Day 2 (#1)

--- pyjdb/__init__.py
+++ pyjdb/__init__.py
@@ -1,7 +1,12 @@
-try:
-    from .day3 import run
-except ImportError:
-    try:
-        from .day2 import run
-    except ImportError:
-        from .day1 import run
+from .day2 import run
+
+
+# try:
+#     from .day3 import run
+# except ImportError:
+#     try:
+#         from .day2 import run
+#         print('Day 2 ka kaam')
+#     except ImportError:
+#         print('Day 1 ka kaam')
+#         from .day1 import run
